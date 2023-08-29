852be50d8f8e7ec74dc6d02919e04148316b5f4d
MBrandt <mareike.brandt@hotmail.de>
2023-08-29 13:39:30 +0900

    feat: added about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="m-10">
+      <h1 className="text-2xl font-semibold mb-5">Welcome to our Help Desk Hub!</h1>
+      <div className="text-gray-700">
+        <p className="mb-4">
+          At our Help Desk, we're dedicated to providing you with exceptional support and assistance. While our FAQ section is currently in the works and will soon be your go-to source for quick answers, our commitment to helping you is already in full swing.
+        </p>
+        <p className="mb-4">
+          Our team is here to listen, understand, and address any questions or concerns you might have. Whether you're facing technical challenges, seeking clarification, or just want to drop us a friendly hello, feel free to use our message submission feature.
+        </p>
+        <br />
+        <p className="mb-4">
+          Your experience matters to us, and we're excited to be your partner on this journey. As we continue to develop our platform, your feedback is invaluable in shaping a better experience for everyone.
+        </p>
+        <p>
+          Thank you for being a part of our Help Desk community. We look forward to assisting you and enhancing your interaction with our services.
+        </p>
+        <br />
+        <p className="mt-4">Best regards,</p>
+        <p>The Help Desk Team</p>
+      </div>
+    </div>
+  );
 };
+
+
+
+
+
+
+
+ 
+
 
 export default About;
